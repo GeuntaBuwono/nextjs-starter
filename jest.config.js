ddba3dd75ed13246ab1743b650079567030b172f
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-04-10 22:47:31 +0700

    chore: takeout babel

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,20 @@
-module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/cypress/'],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/setupTest.ts'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@components(.*)$': '<rootDir>/src/components$1',
-    '^@lib(.*)$': '<rootDir>/src/lib$1',
     '^@pages(.*)$': '<rootDir>/src/pages$1'
-  }
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/cypress/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
+
+module.exports = createJestConfig(customJestConfig);
